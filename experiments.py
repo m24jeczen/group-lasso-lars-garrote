48f7257b6e8e396ae760fa9baae9516d241e0525
Magdalena Jeczeń <magdalena.jeczen.studia@gmail.com>
2025-06-03 16:38:59 +0200

    plots added, experiments updated as a function

--- experiments.py
+++ experiments.py
@@ -5,10 +5,9 @@ from data_generation import generate_data
 from group_lasso import group_lasso, select_lambda_group_lasso_target_groups
 from group_lars import group_lars
 from group_garrote import group_non_negative_garrote_full
+import matplotlib.pyplot as plt
 
-true_groups = {0, 2, 4} 
-
-def evaluate_selection(beta, group_indices, true_groups):
+def evaluate_selection(beta, group_indices, true_groups={0, 2, 4}):
     selected = set()
     for i, idxs in enumerate(group_indices):
         if np.linalg.norm(beta[idxs]) > 1e-6:
@@ -21,46 +20,44 @@ def evaluate_selection(beta, group_indices, true_groups):
     fpr = fp / (fp + tn) if (fp + tn) > 0 else 0
     return tpr, fpr
 
-results = {
-    "Method": ["Group Lasso", "Group LARS", "Group Garrote"],
-    "MSE": [0.0, 0.0, 0.0],
-    "TPR": [0.0, 0.0, 0.0],
-    "FPR": [0.0, 0.0, 0.0]
-}
+def perform_experiment(n_repeats=100, true_groups={0, 2, 4}):
+    all_results = {
+        "Method": [],
+        "MSE": [],
+        "TPR": [],
+        "FPR": []
+    }
 
-n_repeats = 5
-for seed in range(n_repeats):
-    X, Y, group_indices = generate_data(seed=seed)
+    for seed in range(n_repeats):
+        print(f"Running repeat {seed + 1}/{n_repeats}...")
+        X, Y, group_indices = generate_data(seed=seed)
 
-    true_group_count = 3  
-    best_lambda = select_lambda_group_lasso_target_groups(X, Y, group_indices, true_group_count)
-    beta_lasso, pred_lasso = group_lasso(X, Y, group_indices, lambda_=best_lambda)
-    mse_lasso = mean_squared_error(Y, pred_lasso)
-    tpr_lasso, fpr_lasso = evaluate_selection(beta_lasso, group_indices, true_groups)
+        true_group_count = 3
+        best_lambda = select_lambda_group_lasso_target_groups(X, Y, group_indices, true_group_count)
+        beta_lasso, pred_lasso = group_lasso(X, Y, group_indices, lambda_=best_lambda)
+        mse_lasso = mean_squared_error(Y, pred_lasso)
+        tpr_lasso, fpr_lasso = evaluate_selection(beta_lasso, group_indices, true_groups)
 
-    beta_lars, pred_lars = group_lars(X, Y, group_indices)
-    mse_lars = mean_squared_error(Y, pred_lars)
-    tpr_lars, fpr_lars = evaluate_selection(beta_lars, group_indices, true_groups)
+        beta_lars, pred_lars = group_lars(X, Y, group_indices)
+        mse_lars = mean_squared_error(Y, pred_lars)
+        tpr_lars, fpr_lars = evaluate_selection(beta_lars, group_indices, true_groups)
 
-    beta_garrote, pred_garrote, _, _ = group_non_negative_garrote_full(X, Y, group_indices)
-    mse_garrote = mean_squared_error(Y, pred_garrote)
-    tpr_garrote, fpr_garrote = evaluate_selection(beta_garrote, group_indices, true_groups)
+        beta_garrote, pred_garrote, _, _ = group_non_negative_garrote_full(X, Y, group_indices)
+        mse_garrote = mean_squared_error(Y, pred_garrote)
+        tpr_garrote, fpr_garrote = evaluate_selection(beta_garrote, group_indices, true_groups)
 
-    results["MSE"][0] += mse_lasso
-    results["MSE"][1] += mse_lars
-    results["MSE"][2] += mse_garrote
+        all_results["Method"] += ["Group Lasso", "Group LARS", "Group Garrote"]
+        all_results["MSE"] += [mse_lasso, mse_lars, mse_garrote]
+        all_results["TPR"] += [tpr_lasso, tpr_lars, tpr_garrote]
+        all_results["FPR"] += [fpr_lasso, fpr_lars, fpr_garrote]
 
-    results["TPR"][0] += tpr_lasso
-    results["TPR"][1] += tpr_lars
-    results["TPR"][2] += tpr_garrote
+    df_all = pd.DataFrame(all_results)
+    df_mean = df_all.groupby("Method").mean().reset_index()
 
-    results["FPR"][0] += fpr_lasso
-    results["FPR"][1] += fpr_lars
-    results["FPR"][2] += fpr_garrote
+    print("\nAverage Results over {} Repeats:".format(n_repeats))
+    print(df_mean)
 
-for metric in ["MSE", "TPR", "FPR"]:
-    results[metric] = [v / n_repeats for v in results[metric]]
+    return df_mean, df_all
 
-results_df = pd.DataFrame(results)
-print("\nAverage Results over {} Repeats:".format(n_repeats))
-print(results_df)
+
+
